2871010ec36a35da612503092ecf5b34e69a4ac1
alexander <ag.saldivar@gmail.com>
2015-08-17 00:22:49 -0700

    refactor endpoint functions into controllers

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,70 +1,27 @@
 var express = require('express');
 var apiRouter = express.Router(); // get an instance of express router
+var articlesController = require('../controllers/articles');
 
 // bring in our model so we can do things to it
 var Article = require('../models/article');
 
 // param middleware is called before use middleware
 // use param to refactor findById code
-apiRouter.param('article_id', function(request, response, next, id) {
-  Article.findById(id, function(error, article) {
-    if (error) console.error('Could not update article b/c:', error);
-
-    request.article = article; // store article in request
-    next(); // callback to move onto next handler
-  });
-});
+apiRouter.param('article_id', articlesController.articleById);
 
 // configure router middleware
 apiRouter.route('/articles')
 
-  .post(function(request, response) {
-    // create a new article based of user data
-    var article = new Article(request.body);
+  .post(articlesController.create)
 
-    // save the article
-    article.save(function(error) {
-      if (error) console.error('Not able to create article b/c:', error);
-
-      response.json({message: 'Article successfully created'});
-    });
-  })
-
-  .get(function(request, response) {
-    Article.find(function(error, articles) {
-      if (error) console.error('Could not retrieve articles b/c:', error);
-
-      response.json(articles);
-    });
-  });
+  .get(articlesController.index);
 
 apiRouter.route('/article/:article_id')
 
-  .get(function(request, response) {
-    response.json(request.article);
-  })
+  .get(articlesController.show)
 
-  .patch(function(request, response) {
-    var data = request.body;
-    var article = request.article
+  .patch(articlesController.update)
 
-    Object.keys(data).forEach(function(key) {
-      article.set(key, data[key]); // set replaces the value of a field with the specified value
-    });
-
-    article.save(function(error) {
-      if (error) console.error('Could not update article b/c:', error);
-
-      response.json({message: 'Article successfully updated'});
-    });
-  })
-
-  .delete(function(request, response) {
-    Article.remove({ _id: request.params.article_id }, function(error) {
-      if (error) console.error('Could not delete article b/c:', error);
-
-      response.json({message: 'Article successfully deleted'});
-    })
-  });
+  .delete(articlesController.destroy);
 
 module.exports = apiRouter;
