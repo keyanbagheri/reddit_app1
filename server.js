820507c4d5895d23790524ab8a13a23682e79dac
alexander <ag.saldivar@gmail.com>
2015-08-16 18:45:09 -0700

    configure npm start script, bring in express, create simple express server, add .gitignore

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var http = require('http');
+var express = require('express'); // bring in express
+var app = express(); // app is an instance of express
 
-http.createServer(function (request, response) {
-  response.end('Everything works!');
-}).listen(3000);
+// http GET endpoint that sends back json
+app.get('/', function (request, response) {
+  response.json({message: 'Success!'});
+});
+
+app.listen(3000); // tell the app to listen on port 3000
 
 console.log('Server is running on port 3000');
