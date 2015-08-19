89880b88af5a443c863ee8b7d25d6f6c6d6d6afd
alexander <ag.saldivar@gmail.com>
2015-08-18 21:25:04 -0700

    initial ui-router configuration and states

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
-angular.module('reddit', [])
+// we need to inject the third-party library, ui-router
+angular.module('reddit', ['ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+    .state('articles', {
+      url: '/',
+      template: '<h3>View all the Articles</h3>'
+    })
+
+    // catchall
+   $urlRouterProvider.otherwise('/');
+
+}])
 
 .controller('redditController', ['$scope', function($scope) {
   $scope.foo = 'hehehe';
