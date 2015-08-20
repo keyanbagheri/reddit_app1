263a51010be9265d8449ef56f8373e68ca4fa6a2
alexander <ag.saldivar@gmail.com>
2015-08-19 23:30:14 -0700

    add data to panel and voting functionality

--- public/controllers/redditController.js
+++ public/controllers/redditController.js
@@ -1,10 +1,21 @@
 angular.module('reddit')
 
 .controller('redditController', ['$scope', '$http', 'apiService', function($scope, $http, apiService) {
+
+  $scope.votes = 0;
+
   apiService.index()
   .then(function(articles) {
     $scope.articles = articles;
   });
+
+  $scope.plusVote = function(articleId) {
+    $scope.votes++;
+  };
+
+  $scope.minusVote = function() {
+    $scope.votes--;
+  };
 }])
 
 .controller('newArticleController', ['$scope', '$http', 'apiService', function($scope, $http, apiService) {
